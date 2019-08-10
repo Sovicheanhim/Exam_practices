1a35b3b88379aeaf57ff442f2ae2213a640252b8
Sovichea Nhim <nhimsovichea18@kit.edu.kh>
2019-08-10 10:00:42 +0700

    First commit

---
+++ Kotlin_practice/src/Print_Integers.kt
@@ -0,0 +1,7 @@
+import java.util.Scanner
+fun main() {
+    val input = Scanner(System.`in`)
+    println("Enter a number :")
+    var number = input.nextInt()
+    print("You entered : $number")
+}
